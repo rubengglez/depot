b7fd748990d35c4e7a0f39dcbf3381e95509fc86
Rubén García Glez <rubengglez@gmail.com>
2020-06-04 22:18:57 +0200

    add order controller to process the cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :orders
   put '/carts/:id/decrement_amount/:line_item', to: 'carts#decrement_amount'
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
